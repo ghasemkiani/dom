71f37597b1070d0c84c165919fcc278eb0ed56f8
Ghasem Kiani <ghasemkiani@gmail.com>
2024-01-24 00:58:47 +0330

    update

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
-import {cutil} from "@ghasemkiani/base";
-import {Obj} from "@ghasemkiani/base";
-
-class DOM extends Obj {
-	static {
-		cutil.extend(this, {
-			get window() {
-				return this.get();
-			},
-		});
-	}
-	static get() {
-		return cutil.global().window;
-	}
-}
-
-export {DOM};
+export * from "./dom.js";
+export * from "./iwdom.js";
