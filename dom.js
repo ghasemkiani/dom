c32640023746e2699be714d0833bc1c4ba184906
Ghasem Kiani <ghasemkiani@gmail.com>
2024-08-31 21:51:15 +0330

    update

--- dom.js
+++ dom.js
@@ -1,47 +1,47 @@
-import {cutil} from "@ghasemkiani/base";
-import {Obj} from "@ghasemkiani/base";
+import { cutil } from "@ghasemkiani/base";
+import { Obj } from "@ghasemkiani/base";
 
 class DOM extends Obj {
-	static {
-		cutil.extend(this, {
-			get window() {
-				return this.get();
-			},
-		});
-	}
-	static get() {
-		return cutil.global().window;
-	}
+  static {
+    cutil.extend(this, {
+      get window() {
+        return this.get();
+      },
+    });
+  }
+  static get() {
+    return cutil.global().window;
+  }
 }
 
 const iwdom = {
-	DOM,
-	_window: null,
-	_document: null,
-	get window() {
-		if (cutil.na(this._window)) {
-			this._window = this.getWindow();
-		}
-		return this._window;
-	},
-	set window(window) {
-		this._window = window;
-	},
-	get document() {
-		if (cutil.na(this._document)) {
-			this._document = this.getDocument();
-		}
-		return this._document;
-	},
-	set document(document) {
-		this._document = document;
-	},
-	getWindow() {
-		return this.DOM.get();
-	},
-	getDocument() {
-		return this.window.document;
-	},
+  DOM,
+  _window: null,
+  _document: null,
+  get window() {
+    if (cutil.na(this._window)) {
+      this._window = this.getWindow();
+    }
+    return this._window;
+  },
+  set window(window) {
+    this._window = window;
+  },
+  get document() {
+    if (cutil.na(this._document)) {
+      this._document = this.getDocument();
+    }
+    return this._document;
+  },
+  set document(document) {
+    this._document = document;
+  },
+  getWindow() {
+    return this.DOM.get();
+  },
+  getDocument() {
+    return this.window.document;
+  },
 };
 
-export {DOM, iwdom};
+export { DOM, iwdom };
